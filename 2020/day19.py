514ec380f8cfb637a2b5904a47e73add0f83eed2
eiyashou <eiyashou1612@gmail.com>
2020-12-19 19:07:17 +0100

    Automated push from aoc.git.

--- 2020/day19.py
+++ 2020/day19.py
@@ -2,6 +2,7 @@ import sys, os, re, math, hashlib, itertools, string
 from collections import namedtuple, defaultdict, Counter
 import numpy as np
 import regex
+from functools import lru_cache
 
 def parse_data(raw):
     r,m = raw.split("\n\n")
@@ -12,14 +13,17 @@ def parse_data(raw):
             rules[i] = rule.replace("\"","").split(" ")
     return rules, m.split("\n")
 
-
+@lru_cache
 def parse_rule(i, rules):
-    rules[i] = rules[i] if isinstance(rules[i], str) else "("+"".join(parse_rule(ptr, rules) for ptr in rules[i])+")"
-    return rules[i]
+    if isinstance(rules[i], str): reutrn rules[i]
+    frame = "({})" if "|" in rules[i] else "{}"
+    return frame.format("".join(parse_rule(ptr, rules) for ptr in rules[i]))
 
 def one(raw):
     rules, msgs = parse_data(raw)
 
+    #Just to make sure it is clean, even though due to how this is set up it will always be clear
+    parse_rule.cache_clear()
     ptr = re.compile(parse_rule(0, rules))
     return sum(bool(ptr.fullmatch(line)) for line in msgs)
 
@@ -28,6 +32,8 @@ def two(raw):
     
     rules[8] =  ["(?P<eight>", "42", "|", "42", "(?&eight))"]
     rules[11] = ["(?P<eleven>", "42", "31", "|", "42", "(?&eleven)", "31", ")"]
-    
+
+    #Just to make sure it is clean, even though due to how this is set up it will always be clear
+    parse_rule.cache_clear()
     ptr = regex.compile(parse_rule(0, rules))
     return sum(bool(ptr.fullmatch(line)) for line in msgs)
--- README.md
+++
@@ -1,6 +0,0 @@
-# Advent of Code
-This is where I keep track of everything.
-
-Progress:
-
-2020 - ** ** **
