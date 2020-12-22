7e4fa3db3a45eaec8e77f4ea7808e2d7c57c75dd
eiyashou <eiyashou1612@gmail.com>
2020-12-22 12:10:36 +0100

    Automated git push.

--- 2020/day22.py
+++ 2020/day22.py
@@ -1,6 +1,8 @@
 from collections import deque
 from itertools import islice
 
+C = deque((-1,))
+
 def parse_data(raw):
     data = [list(map(int,lines.split("\n")[1:])) for lines in raw.split("\n\n")]
     return data
@@ -20,33 +22,30 @@ def one(raw):
     winner = A if A else B
     return sum(a*i for i,a in enumerate(reversed(winner),1))
 
-def recursive_game(A,B):
-    prevA, prevB = set(), set()
+def recursive_game(A,B,i=False):
+    prev = set()
+    winner = None
     while A and B:
 
-        tA = tuple(A)
-        tB = tuple(B)
-        if tA in prevA and tB in prevB:
-            return True, sum(a*i for i,a in enumerate(reversed(A),1))
+        if (t:=hash(tuple(A)+C+tuple(B))) in prev:
+            winner = True
+            break
         else:
-            prevA.add(tA)
-            prevB.add(tB)
+            prev.add(t)
         
         a = A.popleft()
         b = B.popleft()
 
-        winner = (a>b) if (len(A) < a or len(B) < b) else recursive_game(deque(islice(A,0,a)), deque(islice(B,0,b)))[0]
+        winner = (a>b) if (len(A) < a or len(B) < b) else recursive_game(deque(islice(A,0,a)), deque(islice(B,0,b)),True)
 
-        if winner:
-            A.append(a)
-            A.append(b)
-        else:
-            B.append(b)
-            B.append(a)
+        if winner: 
+            A.extend((a,b))
+        else: 
+            B.extend((b,a))
     
-    return bool(A), sum(a*i for i,a in enumerate(reversed(A+B),1))
+    if not i: winner = sum(i*a for i,a in enumerate(reversed(A if winner else B),1))
+    return winner
 
 def two(raw):
-    A,B = parse_data(raw)
-    winner = recursive_game(deque(A), deque(B))[1]
-    return winner
+    A,B = map(deque,parse_data(raw))
+    return recursive_game(A, B)
